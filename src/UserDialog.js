042522bb39dc55a34d71ae90afda14c045942e99
Jarvis <223385538@qq.com>
2017-06-07 23:11:52 +0800

    把登录注册的form的input，与state.formData绑定

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './UserDialog.css'
+
 export default class UserDialog extends Component {
 
   constructor(props){
     super(props)
     this.state = {
-      selected: 'signUp'
+      selected: 'signIn',
+      formData: {
+      username: '',
+      password: '',
+      }
     }
   }
   
@@ -15,33 +20,28 @@ export default class UserDialog extends Component {
     })
   }
 
+  signUp(e){}
+  signIn(e){}
+
+  changeFormData(key, e){
+    let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
+    stateCopy.formData[key] = e.target.value
+    this.setState(stateCopy)
+  }
+
   render() {
 
-    let signUpForm = (
-      <form className="signUp"> {/* 注册*/}
-        <div className="row">
-          <label>用户名</label> 
-          <input type="text"/>
-        </div>
-        <div className="row">
-          <label>密码</label>
-          <input type="password"/>
-        </div>
-        <div className="row actions">
-          <button type="submit">注册</button>
-        </div>
-      </form>
-    )
-
     let signInForm = (
-      <form className="signIn"> {/* 登录*/}
+      <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
         <div className="row">
           <label>用户名</label>
-          <input type="text"/>
+          <input type="text" value={this.state.formData.username}
+                 onChange={this.changeFormData.bind(this, 'username')} />
         </div>
         <div className="row">
           <label>密码</label>
-          <input type="password"/>
+          <input type="password" value={this.state.formData.password}
+                 onChange={this.changeFormData.bind(this, 'password')} />
         </div>
         <div className="row actions">
           <button type="submit">登录</button>
@@ -49,12 +49,30 @@ export default class UserDialog extends Component {
       </form>
     ) 
 
+    let signUpForm = (
+      <form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
+        <div className="row">
+          <label>用户名</label> 
+          <input type="text" value={this.state.formData.username}
+                 onChange={this.changeFormData.bind(this, 'username')} />
+        </div>
+        <div className="row">
+          <label>密码</label>
+          <input type="password" value={this.state.formData.password} 
+                 onChange={this.changeFormData.bind(this, 'password')} />
+        </div>
+        <div className="row actions">
+          <button type="submit">注册</button>
+        </div>
+      </form>
+    )
+
     return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
           <nav onChange={this.switch.bind(this)}>
-            <input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册
-            <input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登录
+            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登录</label>
+            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册</label>
           </nav>
           <div className="panes">
             {this.state.selected === 'signUp' ? signUpForm : null}
