f028f54572adcea55aeb8ff2f9beb3fe122799da
Jarvis <223385538@qq.com>
2017-06-07 21:55:03 +0800

    add LeanCloude initialization code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import './App.css';
 import TodoInput from './TodoInput'
 import TodoItems from './TodoItems'
 
+import AV from 'leancloud-storage'
+var APP_ID = 'MW4SAI1x8PoEEWyox4Kw1ws7-gzGzoHsz';
+var APP_KEY = '2tXt4IdtN0xTTkC48lvvgk8P';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
 class App extends Component {
   constructor(props) {
     super(props)
